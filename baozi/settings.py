e51a865e06cdc174f15dac3b76db5829a35de05a
Prakahs kumar <90895129+prakash6200@users.noreply.github.com>
2022-10-12 22:17:48 +0530

    Update settings.py

--- baozi/settings.py
+++ baozi/settings.py
@@ -162,8 +162,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-STATIC_URL = '/django-static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
